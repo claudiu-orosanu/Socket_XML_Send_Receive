6f965c723c7cbcb5829c0d9c1fe076282ed76791
Orosanu Claudiu <orosanu.claudiu@gmail.com>
2016-07-18 12:55:39 +0300

    add console app

---
+++ Socket_XML_Send_Receive_Console_App/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Socket_XML_Send_Receive_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var server2 = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                string ip_ext = args[0]; // TO DO
+                int port_send_ext = 10000;
+                string encoding = "ASCII";
+                string content = "asdsa";
+                bool shouldAddLengthPrefix = false;
+                try
+                {
+                    IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse(ip_ext), port_send_ext);
+                    server2.Connect(serverEndPoint);
+                    Console.WriteLine("CLIENT: conectat la server socket <" + ip_ext + ":" + port_send_ext.ToString() + ">");
+                    var bytesToSend = StringConverter.GetBytesToSend(encoding
+                                                  , content
+                                                  , shouldAddLengthPrefix);
+                    server2.Send(bytesToSend, 0, bytesToSend.Length, SocketFlags.None);
+                    Console.WriteLine("CLIENT: date expediate de la client la server socket.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("CLIENT: probleme conectare/trimitere de la client la server socket <" + ip_ext + ":" + port_send_ext.ToString() + ">");
+                    //Debug(ex.ToString());
+                }
+                finally
+                {
+                    if (server2 != null)
+                    {
+                        server2.Close();
+                        ((IDisposable)server2).Dispose();
+                        Console.WriteLine("CLIENT: deconectat de la server socket");
+                    };
+                };
+
+            }
+        }
+    }
+}
---
+++ Socket_XML_Send_Receive_Console_App/StringConverter.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Xml.Schema;
+
+namespace Socket_XML_Send_Receive_Console_App
+{
+    public class StringConverter
+    {
+        public static byte[] GetBytesToSend(string encoding
+                                    , string content
+                                    , bool shouldAddLengthPrefix)
+        {
+            byte[] bytesToSend = GetContentBytes(encoding, content);
+            if (shouldAddLengthPrefix)
+            {
+                int contentLength = bytesToSend.Length;
+                int reqLenH2N = IPAddress.HostToNetworkOrder(contentLength);
+                byte[] reqLenArray = BitConverter.GetBytes(reqLenH2N);
+
+                byte[] buff_intermediar = new byte[contentLength + 4];
+                reqLenArray.CopyTo(buff_intermediar, 0);
+                bytesToSend.CopyTo(buff_intermediar, 4);
+                bytesToSend = buff_intermediar;
+            }
+
+            return bytesToSend;
+        }
+
+        private static byte[] GetContentBytes(string enconding, string content)
+        {
+            byte[] contentBytes = null;
+            switch (enconding)
+            {
+                case "ASCII":
+                    contentBytes = Encoding.ASCII.GetBytes(content);
+                    break;
+                case "UTF7":
+                    contentBytes = Encoding.UTF7.GetBytes(content);
+                    break;
+                case "UTF8":
+                    contentBytes = Encoding.UTF8.GetBytes(content);
+                    break;
+                case "Unicode":
+                    contentBytes = Encoding.Unicode.GetBytes(content);
+                    break;
+                default:
+                    //
+                    break;
+            };
+            return contentBytes;
+        }
+
+        public static bool TryGetStringFromBytes(byte[] rcvBuffer_partial, int bytesReceived, bool shouldValidateSchema,
+                                           out string result, string encoding, string schemaValidationText, IForm form)
+        {
+            result = null;
+            if (shouldValidateSchema && !form.Validation(schemaValidationText))
+            {
+                return false;
+            }
+            result = GetEncoding(encoding).GetString(rcvBuffer_partial, 0, bytesReceived);
+            return true;
+        }
+
+        private static Encoding GetEncoding(string encodingType)
+        {
+            switch (encodingType)
+            {
+                case "ASCII":
+                    return Encoding.ASCII;
+                case "UTF7":
+                    return Encoding.UTF7;
+                case "UTF8":
+                    return Encoding.UTF8;
+                case "Unicode":
+                    return Encoding.Unicode;
+                default:
+                    throw new ArgumentException("Parametrul encodingType nu este bun", nameof(encodingType));
+            }
+        }
+
+    }
+
+    public interface IForm
+    {
+        void MyValidationEventHandler(object sender, ValidationEventArgs args);
+        bool Validation(string file);
+    }
+}
